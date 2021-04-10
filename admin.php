c05fe77a1819510c54b8bd295925a60967a9a192
Daniel Simpkins <dpsimpkins@gmail.com>
2021-04-10 14:16:47 -0400

    Added automatic appointments for new batches.

--- admin.php
+++ admin.php
@@ -20,15 +20,37 @@
       $db->close();
     }
 
-    function getNumOfDoses($batchId, $db) {
-      $num_of_doses_query = "SELECT COUNT(Tracking_no) FROM Dose WHERE Dose.BatchId = {$batchId}";
-      $result = $db->query($num_of_doses_query);
+    function getDoses($batchId, $db) {
+      $get_doses_query = "SELECT Tracking_no FROM Dose WHERE Dose.Batch_no = {$batchId}";
+      $result = $db->query($get_doses_query);
       if(!$result) die($db->error);
-      if($result && $result->num_rows == 1) {
-        $row = $result->fetch_array();
-        return $row[0];
-      } else {
-        return 0;
+      if($result && $result->num_rows > 0) {
+        return $result;
+      }
+      return null;
+    }
+
+    function getWaitlistedPatientsByRow($num_of_patients, $db) {
+      $waitlisted_patients_query = "SELECT P_id, Earliest_date FROM patient WHERE Waitlisted = TRUE ORDER BY Priority LIMIT {$num_of_patients}";
+      $result = $db->query($waitlisted_patients_query);
+      if($result && $result->num_rows > 0) {
+        return $result;
+      }
+      return null;
+    }
+
+    function createAppointment($pid, $tracking_no, $date, $db) {
+      try {
+        $db->begin_transaction();
+        $db->query("INSERT INTO appointment (P_id, Dose_no, Date) VALUES ({$pid}, {$tracking_no}, \"{$date}\")");
+        $db->query("UPDATE patient SET Waitlisted = FALSE WHERE P_id = {$pid}");
+        // TODO: Might need to reserve dose
+        $db->commit();
+        // TODO: This is for testing. Please remove in final release.
+        echo "<div>Made appointment for {$pid} on {$date}</div>";
+      } catch (\Throwable $e) {
+        $db->rollback();
+        throw $e;
       }
     }
 
@@ -37,16 +59,26 @@
       $db = connectToDatabase();
 
       // Get num of doses in batch
-      $doses = getNumOfDoses($batchId, $db);
-      echo $doses;
+      $doses = getDoses($batchId, $db);
+      if(!$doses) return;
 
       // Get patients on waitlist sorted by priority
+      $patients = getWaitlistedPatientsByRow($doses->num_rows, $db);
+      if(!$patients) return;
 
       // Assign first x patients to a batch at their earliest date.
+      while($patient = $patients->fetch_assoc()) {
+        $dose = $doses->fetch_assoc();
+        // TODO: How should the date be chosen?
+        createAppointment($patient["P_id"], $dose["Tracking_no"], $patient["Earliest_date"], $db);
+      }
 
       // Close database connection
       closeDatabaseConnection($db);
     }
+
+    // TODO: For testing purposes. Feel free to delete.
+    scheduleWaitlistedToBatch(1);
     ?>
   </body>
 </html>

db63bef6c279306226e91d588d8b83aeb0861da9
Daniel Simpkins <dpsimpkins@gmail.com>
2021-04-10 15:12:39 -0400

    Added navigation

--- admin.php
+++ admin.php
@@ -4,6 +4,11 @@
     <title>BUR Admin Page</title>
     <meta charset="UTF-8" />
     <link href="main_styles.css" type="text/css" rel="stylesheet" />
+    <script type="text/javascript">
+    function redirect(newPath) {
+      window.location.href = `${window.location.protocol}//${window.location.hostname}:${window.location.port}/${newPath}`;
+    }
+    </script>
   </head>
   <body>
     <nav>
@@ -11,75 +16,12 @@
     </nav>
     <div class="main-content">
       <a href="index.php">Go back</a>
-      <?php
-      function connectToDatabase() {
-        // TODO: Is this database name correct?
-        $conn = new mysqli("localhost", "root", "mysql", "bur");
-        if($conn->connect_error) die($conn->connect_error);
-        if($conn === false) die("Error: Could not connect".mysqli_connect_error());
-        return $conn;
-      }
-
-      function getDoses($batchId, $db) {
-        $get_doses_query = "SELECT Tracking_no FROM Dose WHERE Dose.Batch_no = {$batchId}";
-        $result = $db->query($get_doses_query);
-        if(!$result) die($db->error);
-        if($result && $result->num_rows > 0) {
-          return $result;
-        }
-        return null;
-      }
-
-      function getWaitlistedPatientsByRow($num_of_patients, $db) {
-        $waitlisted_patients_query = "SELECT P_id, Earliest_date FROM patient WHERE Waitlisted = TRUE ORDER BY Priority LIMIT {$num_of_patients}";
-        $result = $db->query($waitlisted_patients_query);
-        if($result && $result->num_rows > 0) {
-          return $result;
-        }
-        return null;
-      }
-
-      function createAppointment($pid, $tracking_no, $date, $db) {
-        try {
-          $db->begin_transaction();
-          $db->query("INSERT INTO appointment (P_id, Dose_no, Date) VALUES ({$pid}, {$tracking_no}, \"{$date}\")");
-          $db->query("UPDATE patient SET Waitlisted = FALSE WHERE P_id = {$pid}");
-          // TODO: Might need to reserve dose
-          $db->commit();
-          // TODO: This is for testing. Please remove in final release.
-          echo "<div>Made appointment for {$pid} on {$date}</div>";
-        } catch (\Throwable $e) {
-          $db->rollback();
-          throw $e;
-        }
-      }
-
-      function scheduleWaitlistedToBatch($batchId) {
-        // Open database connection
-        $db = connectToDatabase();
-
-        // Get num of doses in batch
-        $doses = getDoses($batchId, $db);
-        if(!$doses) return;
-
-        // Get patients on waitlist sorted by priority
-        $patients = getWaitlistedPatientsByRow($doses->num_rows, $db);
-        if(!$patients) return;
-
-        // Assign first x patients to a batch at their earliest date.
-        while($patient = $patients->fetch_assoc()) {
-          $dose = $doses->fetch_assoc();
-          // TODO: How should the date be chosen?
-          createAppointment($patient["P_id"], $dose["Tracking_no"], $patient["Earliest_date"], $db);
-        }
-
-        // Close database connection
-        $db->close();
-      }
-
-      // TODO: For testing purposes. Feel free to delete.
-      scheduleWaitlistedToBatch(1);
-      ?>
+      <div class="grid">
+        <div class="action-card" onclick="redirect('admin/add_batch.php')">
+          <h2>Add Batch</h2>
+          <p>Add a batch of vaccine doses to the system and schedule waitlisted patients</p>
+        </div>
+      </div>
     </div>
   </body>
 </html>

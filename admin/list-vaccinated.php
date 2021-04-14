0660d9503fbd795199f5d4a413df32255625ebdf
Evan Huang <evanhuang117@gmail.com>
2021-04-14 18:43:26 -0400

    add order by to table

--- admin/list-vaccinated.php
+++ admin/list-vaccinated.php
@@ -4,6 +4,17 @@
         <title>List vaccinated patients</title>
         <meta charset="UTF-8" />
         <link href="main_styles.css" type="text/css" rel="stylesheet" />
+    <style>
+        table {
+            cell-spacing: 20px;
+            border-collapse: collapse; 
+        }   
+        td, th {
+            text-align: center;
+            padding: 5px;
+            border: 1px solid black;
+        } 
+    </style>  
     </head>
     <body>
         <div class="main-content">
@@ -21,7 +32,7 @@
 <?php
 function connectToDatabase() {
     //TODO: Is this database name correct?
-        $conn = new mysqli("localhost", "bur", "bur", "BUR_webpage");
+    $conn = new mysqli("localhost", "bur", "bur", "BUR_webpage");
     if($conn->connect_error) die($conn->connect_error);
     if($conn === false) die("Error: Could not connect".mysqli_connect_error());
     return $conn;
@@ -30,12 +41,13 @@ function connectToDatabase() {
 
 function listVaccinated($db){
     $sql = <<<EOD
-                            select p.name as name, p.Ssn as ssn, a.Date as date, b.Manufacturer as manufacturer
-                            from PATIENT as p, APPOINTMENT as a, DOSE as d, BATCH as b
-                            where p.Ssn=a.P_Ssn
-                            and a.Tracking_no=d.Tracking_no
-                            and d.Batch_no=b.Batch_no
-                            and d.Status='used'
+        select p.name as name, p.Ssn as ssn, a.Date as date, b.Manufacturer as manufacturer
+        from PATIENT as p, APPOINTMENT as a, DOSE as d, BATCH as b
+        where p.Ssn=a.P_Ssn
+        and a.Tracking_no=d.Tracking_no
+        and d.Batch_no=b.Batch_no
+        and d.Status='used'
+        order by p.name
 EOD;
     $result = $db->query($sql);
     while ($patient = $result->fetch_array()){
@@ -58,4 +70,4 @@ listVaccinated($db);
     </table>
     </div>
     </body>
-</html>
+    </html>

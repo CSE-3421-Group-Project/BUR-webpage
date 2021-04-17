d4b56de60585add4aad0c9207ab0b2457ac5aa13
dCurren901 <81867577+dCurren901@users.noreply.github.com>
2021-04-17 12:05:31 -0400

    Add files via upload
    Patient.php should work with my own code.  I know that the form does appear.  I am having an issue with the waitlist number, but we can discuss today

---
+++ Patient.php
@@ -0,0 +1,30 @@
+<html>
+<body>
+
+<?php
+print <<<_HTML_
+<h1> BUR Patient Sign-In </h1>
+<form action = PatientProcess.php method="post">
+		Enter Your First Name: <input type="text" name="Fname" placeholder = "First Name"><br><br><br>
+		Enter Your Last Name: <input type="text" name ="Lname" placeholder = "Last Name"><br><br><br>
+		Age: <input type="text" name = "Age" placeholder = "Age"><br><br><br>
+		Phone: <input type="text" name="Phone" placeholder = "Phone"><br><br><br>
+		SSN: <input type="text" name ="SSN" placeholder = "SSN"><br><br><br>
+		<br>
+	
+		<label for="PriorityList">Select a priority group: </label>
+		<select name="PriorityList" id="PriorityList">
+		<option value= "priority">Group 1</option>
+		<option value= "priority">Group 2</option>
+		<option value= "priority">Group 3</option>
+		</select>
+		<br><br><br>
+	    <input type="text" name="Date" placeholder = "Earliest Date"><br><br>
+		<input type="submit">
+
+	</form>
+_HTML_;
+?>
+
+</body>
+</html>

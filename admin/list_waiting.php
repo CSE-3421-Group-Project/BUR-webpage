9d8bb91946add66b398da01207be722d0fff221d
Michael Kwon Griswold <michael.k.griswold@gmail.com>
2021-04-17 13:12:33 -0400

    Added button to create lists for scheduled and waiting patients

---
+++ admin/list_waiting.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>List Waiting Patients</title>
+        <meta charset="UTF-8" />
+        <link href="main_styles.css" type="text/css" rel="stylesheet" />
+    <style>
+        table {
+            cell-spacing: 20px;
+            border-collapse: collapse; 
+        }   
+        td, th {
+            text-align: center;
+            padding: 5px;
+            border: 1px solid black;
+        } 
+    </style>  
+    </head>
+    <body>
+        <div class="main-content">
+            <a href="../admin.php">Go back</a>
+            <h2>Waiting Patients</h2>
+
+            <table>
+                <tr>
+                    <th>Name</th>
+                    <th>Priority</th>
+                    <th>SSN</th>
+                </tr>
+
+<?php
+function connectToDatabase() {
+    $conn = new mysqli("localhost", "bur", "bur", "BUR_webpage");
+    if($conn->connect_error) die($conn->connect_error);
+    if($conn === false) die("Error: Could not connect".mysqli_connect_error());
+    return $conn;
+
+}
+
+function listWaiting($db){
+    $sql = <<<EOD
+        select p.name as name, p.Priority as priority, p.Ssn as ssn, a.Date as date, b.Manufacturer as manufacturer
+        from PATIENT as p,
+        where p.Waitlist = true
+        order by p.Priority
+EOD;
+    $result = $db->query($sql);
+    while ($patient = $result->fetch_array()){
+        $name = $patient['name'];
+        $priority = $patient['priority'];
+        $ssn = $patient['ssn'];
+        echo "<tr>";
+        echo "<td>".$name."</td>";
+        echo "<td>".$priority."</td>";
+        echo "<td>".$ssn."</td>";
+        echo "</tr>";
+    }
+}
+
+$db = connectToDatabase();
+listWaiting($db);
+?>
+    </table>
+    </div>
+    </body>
+    </html>
